309a352116cafbac3e5316d2caaee8fda6e9711b
yeowok <5087pj@gmail.com>
2018-12-20 15:41:32 +0800

    first commit

---
+++ app/src/main/java/com/example/user/myapplication/data.java
@@ -0,0 +1,12 @@
+package com.example.user.myapplication;
+
+public class data {
+    Result results;
+    class Result {
+        Results[] result;
+        class Results {
+            String Station;
+            String Destination;
+        }
+    }
+}
